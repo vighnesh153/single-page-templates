99dbce6dec5777716e78467ba8377e6715fd85d9
vighnesh153 <vighnesh.raut13@gmail.com>
2020-08-27 11:05:47 +0530

    add is_prime

--- python_cp.py
+++ python_cp.py
@@ -32,6 +32,26 @@ def is_digit(character):
     return character in "0123456789"
 
 
+def is_prime(n):
+    if n <= 3:
+        return n > 1
+    
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    
+    return True
+
+
+for i in range(2, 20):
+    print(i, is_prime(i))
+
+
 class This:
     def __init__(self):
         self.max = self.min = self.sum = self.count = None
